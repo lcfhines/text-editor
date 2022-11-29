b646f72a4b198b3f621fe91466f7526e59314caa
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-11-28 22:08:49 -0500

    imported putDb and getDb in index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,7 @@
 import { Workbox } from 'workbox-window';
 import Editor from './editor';
-import './database';
+// need to get and post data 
+import { putDb, getDb } from './database';
 import '../css/style.css';
 
 const main = document.querySelector('#main');
